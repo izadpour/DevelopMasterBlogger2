f488f991d416e2ca2623aab9b0e7fa970000ce8d
t <t@DESKTOP-U2QKNRE>
2021-08-16 20:51:54 +0430

    add-bootstrapping

---
+++ MasterBlogger/MB.Presentation.MVCCore/Areas/Administrator/Pages/ArticleCategoryManagement/List.cshtml.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MB.Application.Contracts.ArticleCategory;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MB.Presentation.MVCCore.Areas.Administrator.Pages.ArticleCategoryManagement
+{
+    public class ListModel : PageModel
+    {
+        public List<ArticleCategoryViewModel> ArticleCategoryViewModels { get; set; }
+        private readonly IArticleCategoryApplication _articleCategoryApplication;
+
+        public ListModel(IArticleCategoryApplication articleCategoryApplication)
+        {
+            _articleCategoryApplication = articleCategoryApplication;
+        }
+
+        public void OnGet()
+        {
+            ArticleCategoryViewModels = _articleCategoryApplication.List();
+        }
+    }
+}
