e9ee8589204e0edff42766fd6c4582a0682086a0
t <t@DESKTOP-U2QKNRE>
2021-08-16 18:14:53 +0430

    add-ArticleCategory

---
+++ MasterBlogger/MB.Application/ArticleCategoryApplication.cs
@@ -0,0 +1,13 @@
+﻿using MB.Application.Contracts.ArticleCategory;
+using System.Collections.Generic;
+
+namespace MB.Application
+{
+    public class ArticleCategoryApplication : IArticleCategoryApplication
+    {
+        public List<ArticleCategoryViewModel> List()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ MasterBlogger/MB.Domain/ArticleCategoryAgg/ArticleCategory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MB.Domain.ArticleCategoryAgg
+{
+   public class ArticleCategory
+    {
+        public long Id { get; private set; }
+        public string Title { get; private set; }
+        public DateTime CreationDate { get; private set; }
+        public bool IsDeleted { get; private set; }
+
+        public ArticleCategory(string title)
+        {
+            Title = title;
+            CreationDate=DateTime.Now;
+            IsDeleted = false;
+        }
+    }
+}
